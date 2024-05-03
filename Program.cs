a71e16d0f7fe46a822a917da38f89f9704fc2892
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-03 02:32:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+
+            Console.WriteLine("Original array:");
+            PrintArray(array);
+
+            BubbleSort(array);
+            // Array.Sort(array);
+
+
+            Console.WriteLine("\nSorted array:");
+            PrintArray(array);
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // swap arr[j] and arr[j+1]
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j +1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implementing my own algorithm for a bubble sort.
+        /// </summary>
+        /// <param name="array"></param>
+        public static void PrintArray(int[] array)
+        {
+            foreach(var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
